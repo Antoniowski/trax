a0fc0e3524d6d2d9b762bf52eac714ed17ab90ae
Antoniowski <r97.antonio@gmail.com>
2026-03-13 11:52:38 +0100

    added doc to tageditor.hpp

--- headers/tageditor.hpp
+++ headers/tageditor.hpp
@@ -2,4 +2,14 @@
 #include <vector>
 #include "MetadataSearcher.hpp"
 
+
+
+/**
+ * @brief 
+ * 
+ * @param songNames vector of files names
+ * @param songsDirPath path of the dir containing audio files
+ * @param metadatas MP3Tag vector pointer that contains the found tags that will be used to edit the files' ones
+ * @param artistName artist name string
+ */
 void editTags(std::vector<std::string> songNames, std::string songsDirPath,  std::vector<MetadataSearcher::MP3Tag>* metadatas, std::string artistName);
