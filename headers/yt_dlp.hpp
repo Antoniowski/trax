0c546f307cbdab07601ad04a2190d62f7ecc37bf
Antoniowski <r97.antonio@gmail.com>
2026-03-13 12:12:25 +0100

    added doc to yt_dlp.hpp

--- headers/yt_dlp.hpp
+++ headers/yt_dlp.hpp
@@ -2,7 +2,27 @@
 #define YT_DLP_H
 #include <string>
 
+/**
+ * @brief handles the download the playlist linked to the specified url using yt-dlp. Album and artist parameter
+ * will be used to create a dir where all the downloaded files will be saved 
+ * 
+ * @param url url of the playlist to download
+ * @param album album name
+ * @param artist artist name
+ * @param debug flag that indicates if the program was launched in debug mode
+ * @return int 
+ */
 int downloadPlaylist(std::string url,std::string album, std::string artist, bool debug = false);
+
+
+/**
+ * @brief handle the download of the audio linked to the url. The audio will be saved in the 
+ * dir where the program was launched.
+ * 
+ * @param url url of the song that will be downloaded
+ * @param debug flag that indicates if the program was launched in debug mode
+ * @return int 
+ */
 int downloadSong(std::string url, bool debug = false);
 
 #endif
