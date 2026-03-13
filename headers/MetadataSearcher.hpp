4b1f43ccf89cdd6f2b5400bf15a388080cffdce6
Antoniowski <r97.antonio@gmail.com>
2026-03-13 11:34:18 +0100

    added doc to metadatasearcher.hpp

--- headers/MetadataSearcher.hpp
+++ headers/MetadataSearcher.hpp
@@ -37,18 +37,84 @@ class MetadataSearcher{
         
         MetadataSearcher();
         ~MetadataSearcher();
+
+
+        /**
+         * @brief handles the query to the MusicBrainz5 database to retrieve tags of all the songs of a specific album
+         * 
+         * @param album album name (case sensitive)
+         * @param artist artist name (case sensitive)
+         * @param year year when the album was released
+         * @return std::vector<MP3Tag>* vector with all the tags of the songs of the first album retrieved
+         */
         std::vector<MP3Tag>* searchAlbum(std::string album, std::string artist, int year = 0);
+
+
+        /**
+         * @brief handles the query to the MusicBrainz5 database to retrieve the tag of a specific song of a specific album
+         * 
+         * @param songName song name (case sensitive)
+         * @param album album name (case sensitive)
+         * @param artist artist name (case sensitive)
+         * @param year year when the album was released
+         * @return MP3Tag* tag of the specified song of the first album retrieved
+         */
         MP3Tag* searchSong(std::string songName, std::string album, std::string artist, int year = 0);
+
+
+        /**
+         * @brief handles the download of the cover art of a specified album
+         * 
+         * @param albumID id of the album of which the program will download the cover art
+         */
         void downloadCoverArt(std::string albumID);
 
     private:
         CQuery::tParamMap params;
         CQuery::tParamMap lookupParams;
 
+
+        /**
+         * @brief Set the Params object
+         * 
+         * @param album album name (case sensitive)
+         * @param artist artist name (case sensitive)
+         * @param year year when the album was released
+         */
         void setParams(std::string album, std::string artist, int year);
+
+
+        /**
+         * @brief Set the Params object
+         * 
+         * @param songName song name (case sensitive)
+         * @param album album name (case sensitive)
+         * @param artist artist name (case sensitive)
+         * @param year year when the album was released
+         */
         void setParams(std::string songName, std::string album, std::string artist, int year);
+
+
+        /**
+         * @brief Set the Lookup Params object
+         * 
+         */
         void setLookupParams();
+
+
+        /**
+         * @brief reset all the params objects
+         * 
+         */
         void resetParams();
+
+
+        /**
+         * @brief retrieve a single string containing all the artist of the specified CArtistCredit object 
+         * 
+         * @param ac pointer containing the CArtistCredit that will be used to retrive the artist string
+         * @return std::string containing all the artist found in the CArtistCredit object
+         */
         std::string readArtists(CArtistCredit* ac);
 };
 #endif
